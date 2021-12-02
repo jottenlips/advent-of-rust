18be99791c12a9587babba08d59db44174076dfb
John Ottenlips <jottenlips@gmail.com>
2021-12-01 21:37:44 -0600

    read me

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # advent-of-rust
 
 https://adventofcode.com/
+
+# run
+
+cargo build && cargo run daynumber
+
+```
+cargo build && cargo run 1
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod day1;
+use std::env;
 
 fn main() {
-    day1::day_1();
+    let args: Vec<String> = env::args().collect();
+    let day = args[1].parse::<u32>().unwrap();
+
+    match day {
+        1 => {day1::day_1();}
+        _ => {println!("Day not found");}
+    }
 }
