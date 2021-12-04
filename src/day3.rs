a6f585b66b94d7b993c8cb18291c89489e9aae59
John Ottenlips <jottenlips@gmail.com>
2021-12-03 23:33:47 -0600

    almost have it

--- src/day3.rs
+++ src/day3.rs
@@ -6,7 +6,6 @@ pub fn day_3() {
         .expect("Something went wrong reading the file");
     let lines = contents.split('\n');
 
-  
     let first_line = contents
       .split_whitespace()
       .next()
@@ -14,9 +13,6 @@ pub fn day_3() {
     let line_length = first_line.len();
 
     let mut counts = vec![0; line_length];
-    // let mut counts:Vec<u32> = [];
-    println!("ll {:?}", counts);
-
 
     let line_count = lines.clone().count() as u32;
 
@@ -26,6 +22,7 @@ pub fn day_3() {
         counts[i] += bit;
       }
     });
+
     let gamma_bin:String = counts.iter().flat_map(|x| {
      return match x > &(line_count/2) {
         true => "1".chars(),
@@ -54,12 +51,73 @@ fn binary_to_decimal(binary: &str) -> u32 {
 
 fn day_3_pt2() {
   
-    // let filename = "day3pt2.txt";
-    // let contents = fs::read_to_string(filename)
-    //     .expect("Something went wrong reading the file");
-    // let lines = contents.split('\n');
+    let filename = "day3pt2.txt";
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    let lines = contents.split('\n');
+   
+    let mut oxygen_ratings:Vec<String> = lines.clone().map(|s| s.to_string()).collect();
+    let mut co2_ratings:Vec<String> = lines.clone().map(|s| s.to_string()).collect();
 
-    
-    // println!("Day 3 pt 2 ans: {:?}", lines);      
+    let first_line = contents
+      .split_whitespace()
+      .next()
+      .unwrap_or("");
+    let line_length = first_line.len();
+
+    let mut counts = vec![0; line_length];
+
+    let line_count = lines.clone().count() as u32;
+
+
+    lines.for_each(|x|{
+      let bits = x.chars().map(|x| x.to_digit(10).unwrap()).collect::<Vec<u32>>();
+      for (i, bit) in bits.iter().enumerate() {
+        counts[i] += bit;
+      }
+    });
+
+
+    for (i, count) in counts.iter().enumerate() {
+      let more_1s = count > &(line_count/2);
+      let more_0s = count < &(line_count/2);
+      let equal_0_1 = count == &(line_count/2);
+      oxygen_ratings = oxygen_ratings.iter().filter(|line| {
+          let has_1 = line.chars().nth(i).unwrap() == '1';
+          let has_0 = line.chars().nth(i).unwrap() == '0';
+          if has_1 && (more_1s || equal_0_1) {
+            return true
+          } 
+          if has_0 && more_0s {
+            return true
+          }
+          if oxygen_ratings.iter().count() == 1 {
+            return true
+          }
+          return false
+      }).cloned().collect();
+      co2_ratings = co2_ratings.iter().filter(|line| {
+          let has_1 = line.chars().nth(i).unwrap() == '1';
+          let has_0 = line.chars().nth(i).unwrap() == '0';
+          
+          if has_0 && more_1s {
+            return true
+          } 
+          if has_1 && (more_0s || equal_0_1) {
+            return true
+          }
+          if co2_ratings.iter().count() == 1 {
+            return true
+          }
+          return false
+        }).cloned().collect();
+
+      println!("{:?} {:?}", oxygen_ratings, co2_ratings);
+    }
+    let oxygen_rating = oxygen_ratings.iter().next().unwrap();
+    let co2_rating = co2_ratings.iter().next().unwrap();
+    let life_support_score = binary_to_decimal(oxygen_rating) * binary_to_decimal(co2_rating);
+
+    println!("Day 3 pt 2 ans: {:?}", life_support_score);
 
 }
